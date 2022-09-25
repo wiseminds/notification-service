0aa94e7d4207d0b385f5bb6e57218284dfad8c2a
Wisdom Ekeh <ekeh.wisdom@gmail.com>
2022-09-25 20:54:01 +0100

    added exit to generator

--- src/generator.ts
+++ src/generator.ts
@@ -1,6 +1,9 @@
 import { config } from "dotenv";
+import { exit } from 'process';
 config();
 
 import { TemplateService } from './services/template.service';
 
 new TemplateService().seedAll()
+
+exit(0);
